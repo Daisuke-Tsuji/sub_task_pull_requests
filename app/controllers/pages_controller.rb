6a53f255581831f97634dbb4fd25dac43d5c332d
Daisuke-Tsuji <kixrsb24@gmail.com>
2020-07-10 05:35:45 +0900

    create status feed in root_path

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,27 @@
 class PagesController < ApplicationController
-  before_action :sign_in_required
+  before_action :sign_in_required, only: [:show, :edit]
+  before_action :set_user, only: [:show]
 
 
   def index
-    @user = User.all.where.not(id: current_user.id)
+    if user_signed_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed
+    else
+      @user = User.all
+    end
   end
 
   def show
-    @user = User.find(current_user.id)
-    @micropost = current_user.microposts
+    @micropost = @user.microposts
   end
 
   def edit
   end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,5 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   mount_uploader :image, ImageUploader
   validates :user_id, presence: true
+  validates :comment, length: { maximum: 2200 }    
 end
