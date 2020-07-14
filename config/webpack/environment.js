5f1a3b79319aafc569fd7ef5bbd79850a406deda
Daisuke-Tsuji <kixrsb24@gmail.com>
2020-07-15 04:25:38 +0900

    organize　_micropost. pages/show root,micropost

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,11 @@
 const { environment } = require('@rails/webpacker')
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
+environment.config.resolve.alias = {'vue$': 'vue/dist/vue.esm.js' }
+// jQueryとBootstapのJSを使えるように
 const webpack = require('webpack')
 environment.plugins.prepend(
   'Provide',
@@ -9,5 +15,4 @@ environment.plugins.prepend(
     Popper: 'popper.js'
   })
 )
-
 module.exports = environment
