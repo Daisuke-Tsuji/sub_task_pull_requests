6ddc779e0768b3503bc977a7c4f0a8ed6aaa4575
Daisuke-Tsuji <kixrsb24@gmail.com>
2020-07-27 22:14:44 +0900

    hide facebook ID by dotenv-gem

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'jquery-rails'
 gem 'font-awesome-sass', '~> 5.13.0'
 gem 'kaminari'
 gem 'ransack'
+gem 'dotenv-rails'
 
 group :development, :test do
   gem 'sqlite3', '1.4.1'
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,5 +309,5 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  config.omniauth :facebook, FACEBOOK_ID='742130306553906', FACEBOOK_SECRET_KEY='025ef99548b00b5f21ff52bcdb00a4ec'
+  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET_KEY']
 end
